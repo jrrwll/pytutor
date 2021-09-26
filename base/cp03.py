e94dc40877d6b16013d7e5c52f87f6c30a60468b
tuke <jrriwll@gmail.com>
2021-09-26 23:58:43 +0800

    commit

--- base/cp03.py
+++ base/cp03.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 
 """
-内存复用
+内存引用和集合
 """
 
 # 斐波拉契数列
@@ -31,3 +31,40 @@ print(a, b, c) # 3 5 8
 # TODO
 print(a, b, c) # 8 3 5
 
+a = [1, 2, 3, 4, 5]
+# a[i] 意思是取a的第i+1个元素
+print(a[0], a[1], a[2], a)
+# a[i] = b，意思是将b赋值给a的第i+1个元素
+a[0] = -1 # 把-1赋值给a的第一个元素
+print(a) # -1 2 3 4 5
+# TODO
+print(a) # 5 4 3 2 -1
+
+# [1, 4, 9, 16, 25, 36, 49, ... n**2]
+a = []
+for i in range(1, 21):
+    # list的方法(函数)，添加一个元素到列表的尾端
+    a.append(i ** 2)
+print(a)
+a.clear()
+
+a = [1, 2, 3]
+a.insert(1, -4) # 插入-4到1位
+b = a.pop(1) # pop 删除i位并且将其返回(第i+1个元素)
+print(a, b)
+
+# // 整除取商，比如 5//2为 2
+x = len(a)
+y = len(a) // 2
+print(y)
+# 有y对元素需要交换，其中 a[i] 需要和 a[x-i-1] 交换
+# i的取值为从0到y-1
+# a[0] --- a[x-1]
+# a[1] --- a[x-2]
+# a[i] --- a[x-i-1]
+for i in range(0, y):
+    # 将a[i] 和 a[x-i-1] 交换
+    m=a[i]
+    a[i]=a[x-i-1]
+    a[x-i-1]=m
+    print(a)
